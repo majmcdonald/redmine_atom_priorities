a43ab7341d673a89cdb5c07ff149c78b334fed96
Matt McDonald <majmcdonald@gmail.com>
2014-07-22 09:39:43 -0500

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_atom_priorities do
+  name 'Redmine Atom Priorities plugin'
+  author 'Matt McDonald'
+  description 'show priorities in rss feed'
+  version '0.0.1'
+end
